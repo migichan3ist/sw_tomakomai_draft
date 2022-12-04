1bc4a72744d8d4a0fe1ee9985f6831b4fb38a93e
migichan3ist <migichan@ist.hokudai.ac.jp>
2022-12-04 10:16:46 +0900

    new

---
+++ js/sorry_json.js
@@ -0,0 +1,10 @@
+console.log("aaaaaaaaaaaaaaaaaa");
+
+const url = 'https://hooks.slack.com/services/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';
+const data = {
+    text: "ユーザーが購入しました"
+};
+const xml = new XMLHttpRequest();
+xml.open("POST", url, false);
+xml.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+xml.send(`payload=${JSON.stringify(data)}`);
